32bb1ea8ffa60d4a93c146c049478db0f72b6f6b
Misha <Mzhebrunov@gmail.com>
2022-02-13 16:40:10 -0500

    Adds the files and code to setup commands.

---
+++ TinuxTerminalEmulator/TinuxTerminalEmulatorUI/CommandFunctions.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinuxTerminalEmulatorUI
+{
+    public static class CommandFunctions
+    {
+        private static string _lastLine = string.Empty;
+
+        //public static void GetLastLine(this FileInfo file)
+        //{
+        //    using (FileStream fileStream = file.OpenRead())
+        //    {
+        //        fileStream.Seek(0, SeekOrigin.End);
+
+        //        var allLines = File.ReadAllLines(file.ToString()).ToList();
+
+        //        using (StreamReader sreader = new StreamReader(fileStream))
+        //        {
+        //            _lastLine = sreader.ReadLine();
+        //        }
+
+        //        if (_lastLine == "")
+        //        {
+        //            using (StreamWriter swriter = file.CreateText())
+        //            {
+        //                _lastLine = _lastLine.Replace("\n", "");
+        //                //swriter.
+        //            }
+        //        }
+        //    }
+        //}
+
+        public static List<int> SubStringPosition(this List<string> stringList, string includes)
+        {
+            int i = 1;
+
+            List<int> lines = new List<int>();
+
+            foreach (string str in stringList)
+            {
+                if (str.Contains(includes))
+                {
+                    lines.Add(i);
+                }
+
+                i++;
+            }
+
+            return lines;
+        }
+
+        public static List<string> GetAllLines(this FileInfo file)
+        {
+            var allLines = File.ReadAllLines(file.ToString()).ToList();
+
+            return allLines;
+        }
+
+        public static void WriteLineToFile(this FileInfo file, string write)
+        {
+            using (StreamWriter swriter = file.CreateText())
+            {
+                swriter.WriteLine(write);
+            }
+        }
+        public static string ReadLineByIndex(this FileInfo file, int index)
+        {
+            using (FileStream fileStream = file.OpenRead())
+            {
+                fileStream.Seek(0, SeekOrigin.Begin);
+
+                using (StreamReader sreader = new StreamReader(fileStream))
+                {
+                    for (int i = 1; i < index; i++)
+                    {
+                        sreader.ReadLine();
+                    }
+
+                    return sreader.ReadLine();
+                }
+            }
+        }
+
+        //public static List<string> ReadLineByListIndex(this FileInfo file, List<int> indexs)
+        //{
+        //    List<string> linesInIndex = new List<string>();
+
+        //    using (FileStream fileStream = file.OpenRead())
+        //    {
+        //        fileStream.Seek(0, SeekOrigin.Begin);
+
+        //        using (StreamReader sreader = new StreamReader(fileStream))
+        //        {
+        //            int line = 1;
+
+        //            foreach (var index in indexs)
+        //            {
+        //                while (line < index)
+        //                {
+        //                    var read = sreader.ReadLine();
+
+        //                    line++;
+        //                }
+
+        //                linesInIndex.Add(sreader.ReadLine());
+
+        //                line = 1;
+
+        //                fileStream.Seek(0, SeekOrigin.Begin);
+        //            }
+
+        //            return linesInIndex;
+        //        }
+        //    }
+        //}
+
+        public static bool Includes(this string str, string includes)
+        {
+            
+            if (str.Contains(includes))
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+        public static string GetLastLine(this FileInfo file)
+        {
+            List<string> lines = File.ReadAllLines(file.ToString()).ToList();
+            using (FileStream fileStream = file.OpenRead())
+            {
+                fileStream.Seek(0, SeekOrigin.Begin);
+                using (StreamReader sreader = new StreamReader(fileStream))
+                {
+                    for (int i = 1; i < lines.Count; i++)
+                    {
+                        sreader.ReadLine();
+                    }
+
+                    return sreader.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ TinuxTerminalEmulator/TinuxTerminalEmulatorUI/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LinuxTerminalEmulatorUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        FileInfo commandDir = new FileInfo(@"C:\LinuxTerminalEmulator\Commands.txt");
+        FileInfo translatedCommandLineDir = new FileInfo(@"C:\LinuxTerminalEmulator\TranslatedCommandLines.txt");
+        FileInfo translatedCommandDir = new FileInfo(@"C:\LinuxTerminalEmulator\TranslatedCommands.txt");
+
+        private void MainTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                //MainTextBox.Text = commandDir.GetLastLine();
+            }
+
+            else if (e.Key == Key.Up)
+            {
+
+            }
+
+            else if (e.Key == Key.Down)
+            {
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo lteDir = new DirectoryInfo(@"C:\LinuxTerminalEmulator");
+
+            if (!Directory.Exists(lteDir.ToString()))
+            {
+                lteDir.Create();
+            }
+            else
+            {
+
+            }
+
+            if (!File.Exists(commandDir.ToString()))
+            {
+                commandDir.Create();
+            }
+            else
+            {
+
+            }
+            if (!File.Exists(translatedCommandLineDir.ToString()))
+            {
+                translatedCommandLineDir.Create();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void MainTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //var code = MainTextBox.Text;
+
+            //using (StreamWriter swriter = commandDir.CreateText())
+            //{
+            //    swriter.WriteLine(code);
+            //}
+        }
+
+        private void MainTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                // First translation
+                string code = MainTextBox.Text;
+
+                commandDir.WriteLineToFile(code);
+
+                // Second Translation
+
+                var lines = commandDir.GetAllLines();
+
+                List<string> corLines = new List<string>();
+
+                using (StreamReader sreader = commandDir.OpenText())
+                {
+                    foreach (var line in lines)
+                    {
+                        if (line.Includes(@"\>"))
+                        {
+                            corLines.Add(line);
+                        }
+                    }
+                }
+
+                //List<string> corLines = commandDir.ReadLineByListIndex(commandLines);
+
+                using (StreamWriter swriter = translatedCommandLineDir.CreateText())
+                {
+                    foreach (var corLine in corLines)
+                    {
+                        corLine.Trim();
+                        swriter.WriteLine(corLine);
+                    }
+                }
+
+                // Third translation
+
+                List<string> commandLines = translatedCommandLineDir.GetAllLines();
+
+                List<string> dirs = new List<string>();
+
+                using (StreamWriter swriter = translatedCommandDir.CreateText())
+                {
+                    List<List<string>> seperatedCommandLineList = new List<List<string>>();
+
+                    foreach (var commandLine in commandLines)
+                    {
+                        seperatedCommandLineList.Add(commandLine.Split("\\> ").ToList());
+                    }
+
+                    foreach (var seperatedCommandLine in seperatedCommandLineList)
+                    {
+                        if (seperatedCommandLine.Count > 2)
+                        {
+                            MainTextBox.Text += "\n";
+                            MainTextBox.Text += @"Error: Cannot have more than 2 \> s";
+                        }
+
+                        dirs.Add(seperatedCommandLine[0]);
+
+                        seperatedCommandLine.RemoveAt(0);
+
+                        foreach (string commandLine in seperatedCommandLine)
+                        {
+                            swriter.WriteLine(commandLine);
+                        }
+                    }
+                }
+
+                var lc = translatedCommandDir.GetLastLine();
+
+                LineModel.LastLineCommand = lc;
+
+                var ld = dirs[dirs.Count - 1];
+
+                ld += "\\";
+
+                LineModel.LastLineDirectory = ld;
+            }
+
+            else if (e.Key == Key.Up)
+            {
+
+            }
+
+            else if (e.Key == Key.Down)
+            {
+
+            }
+        }
+    }
+}
